fd8aee795b77ae678335aee900981a792f758d21
kenny813x201 <kenny813x201@gmail.com>
2019-07-01 16:49:15 +1000

    Add items

--- etf/etf/items.py
+++ etf/etf/items.py
@@ -11,4 +11,14 @@ import scrapy
 class EtfItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    ticker = scrapy.Field()
+    url = scrapy.Field()
+    fund_name = scrapy.Field()
+    fund_type = scrapy.Field()
+    mgmt_type = scrapy.Field()
+    fund_manager = scrapy.Field()
+    market_cap = scrapy.Field()
+    mgmt_cost = scrapy.Field()
+    performance_fees = scrapy.Field()
+    inception_date = scrapy.Field()
     pass
--- etf/etf/spiders/etfwatch.py
+++ etf/etf/spiders/etfwatch.py
@@ -1,8 +1,10 @@
 import scrapy
+from ..items import EtfItem
 
 
 class etfWatchSpider(scrapy.Spider):
     name = "etf-watch"
+    
 
     def start_requests(self):
         urls = [
@@ -12,6 +14,8 @@ class etfWatchSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        items = EtfItem()
+
         content = response.body
         file_name = 'output/etf-watch.html'
         self.save_html(file_name, content)
@@ -19,11 +23,28 @@ class etfWatchSpider(scrapy.Spider):
         rows = response.xpath("//table/tbody/tr")
         for row in rows:
             ticker = row.xpath("./td[1]/a/text()").extract_first().strip()
+            url = row.xpath("./td[1]/a/@href").extract_first().strip()
             fund_name = row.xpath("./td[2]/a/text()").extract_first().strip()
+            fund_type = row.xpath("./td[3]/span/text()").extract_first().strip()
+            mgmt_type = row.xpath("./td[4]/span/text()").extract_first().strip()
+            fund_manager = row.xpath("./td[5]/span/text()").extract_first().strip()
+            market_cap = row.xpath("./td[6]/span/text()").extract_first().strip()
+            mgmt_cost = row.xpath("./td[7]/span/text()").extract_first().strip()
+            performance_fees = row.xpath("./td[8]/span/text()").extract_first().strip()
+            inception_date = row.xpath("./td[9]/span/text()").extract_first().strip()
 
-            yield {
-                "ticket": ticker,
-                "fund_name": fund_name}
+            items['ticker'] = ticker
+            items['url'] = url
+            items['fund_name'] = fund_name
+            items['fund_type'] = fund_type
+            items['mgmt_type'] = mgmt_type
+            items['fund_manager'] = fund_manager
+            items['market_cap'] = market_cap
+            items['mgmt_cost'] = mgmt_cost
+            items['performance_fees'] = performance_fees
+            items['inception_date'] = inception_date
+            
+            yield items
 
     def save_html(self, file_name, content):
         with open(file_name, 'wb') as f:
